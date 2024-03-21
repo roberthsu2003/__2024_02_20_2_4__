e81f22ad7b8ff88906d8c4d59176651703169a7a
徐國堂 <roberthsu2003@gmail.com>
2024-03-21 13:37:03 +0000

    modify

--- lesson10/lesson10_2.py
+++ lesson10/lesson10_2.py
@@ -1,10 +1,12 @@
-from tools import get_names
+from tools import get_names,get_scores
 import pyinputplus as pyip
+from pprint import pprint
 
 def main():
     nums:int = pyip.inputInt("請輸入學生數(1~30):",min=1,max=30)
     names:list[str] = get_names(nums=nums)
-    print(names)
+    scores = get_scores(names)
+    pprint(scores)
 
 if __name__ == '__main__':
     main()
--- lesson10/tools.py
+++ lesson10/tools.py
@@ -6,3 +6,15 @@ def get_names(nums:int = 5) -> list[str]:
         names:list[str] = content.split(sep='\n')
         random_names:list[str] = random.choices(names,k=nums)
     return random_names
+
+
+def get_scores(names:list[str]) -> list[dict]:
+    stus:list[dict] = []
+    for name in names:
+        stu:dict[str:any] = {}
+        stu["name"] = name
+        stu["chinese"] = random.randint(50,100)
+        stu["english"] = random.randint(50,100)
+        stu["math"] = random.randint(50,100)
+        stus.append(stu)
+    return stus
